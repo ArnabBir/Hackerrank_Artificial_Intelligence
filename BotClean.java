4b027b3602d86bb29ec659af688861e5f109709b
Arnab Bir <arnabbir@gmail.com>
2016-10-06 15:59:55 +0530

    Update BotClean.java

--- BotClean.java
+++ BotClean.java
@@ -6,25 +6,26 @@ import java.util.regex.*;
 
 public class Solution {
     static void next_move(int posr, int posc, String[] board){
-        int [] xpos, ypos, distance = new int [board.length];
-        //int ypos[board.length], distance[board.length];
-        int min_index = -1;
+        int [] xpos = new int [board.length];
+        int [] ypos = new int [board.length];
+        int [] distance = new int [board.length];
+        int min_index = 0;
         for (int i = 0; i < board.length; ++i ){
             if(board[i].contains("b")){ posr = i; posc = board[i].indexOf("b"); break;}
-            xpos[i] = -1;
-            ypos[i] = -1;
         }
         for (int i = 0; i < board.length; ++i ){
-            xpos[i] = -1;
-            ypos[i] = -1;
-            if(board[i].contains("d") && ypos[i] == -1){ ypos[i] = i; xpos[i] = board[i].indexOf("d"); break;}
+            xpos[i] = 0;
+            ypos[i] = 0;
+            distance[i] = 99999999;
+            if(board[i].contains("d") && ypos[i] == 0){ ypos[i] = i; xpos[i] = board[i].indexOf("d");}
             distance[i] = Math.abs(xpos[i] - posr) + Math.abs(ypos[i] - posc);
             if(distance[min_index] > distance[i]) min_index = i;
         }
-        if( posr < xpos[min_index] && posr != xpos[min_index]){ System.out.println("RIGHT"); ++posr; return;}
-        if( posr > xpos[min_index] && posr != xpos[min_index]){ System.out.println("LEFT"); --posr; return;}
-        if( posc < ypos[min_index] && posc != ypos[min_index]){ System.out.println("DOWN"); ++posc; return;}
-        if( posc > ypos[min_index] && posc != ypos[min_index]){ System.out.println("UP"); --posc; return;}
+        if( posr < xpos[min_index]){ System.out.println("RIGHT"); ++posr; return;}
+        else if( posr > xpos[min_index]){ System.out.println("LEFT"); --posr; return;}
+        else if( posc < ypos[min_index]){ System.out.println("DOWN"); ++posc; return;}
+        else if( posc > ypos[min_index]){ System.out.println("UP"); --posc; return;}
+        else{System.out.println("CLEAN");}
         
     }
     public static void main(String[] args) {
